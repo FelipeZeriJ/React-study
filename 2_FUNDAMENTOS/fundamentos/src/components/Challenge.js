f4e766e628f270738619caa1d3a46ca79ae47aa6
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-07 13:39:58 -0500

    ainda trabalhando no desafio sessão 2 tarefa 2

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/Challenge.js
@@ -0,0 +1,18 @@
+const Challenge = () => {
+    // const somar = () => {
+    //     let n1 = <h1> coloque um numero 1: <input type="Number" /></h1>
+    //     let n2 = <h1> coloque um numero 2: <input type="Number" /></h1>
+    //     return n1 + n2
+    // }
+
+    // let n1 = 9
+    // let n2 = 3
+    // let somar = n1 + n2
+    return (
+        <div>
+            <button>Somar</button>
+        </div>
+    )
+}
+
+export default Challenge
