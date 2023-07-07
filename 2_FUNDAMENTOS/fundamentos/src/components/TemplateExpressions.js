2126d462391f10d1f2f1f1e3a2754fb1f88bbd75
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-07 11:51:54 -0500

    extras aula 24

--- 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -1,9 +1,16 @@
 const TemplateExpressions = () => {
     const name = "Felipe Zeri";
+    const data = {
+        age:31,
+        job: "Programmer",
+    }
 
     return (
         <div>
             <h1>Olá {name}, tudo bem?</h1>
+            <p>Você atua como: {data.job}</p>
+            <p>{4 + 4}</p>
+            <p>{console.log("JSX React")}</p>
         </div>
     )
 }
