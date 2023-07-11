1797aee0bcd40a9de8833eca6d819ddaa03d5e62
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-11 13:39:12 -0500

    aprendendo deletar id ramdom

--- 3_AVANÇANDO-EM-REACT/avancando_em_react/src/components/ListRender.js
+++ 3_AVANÇANDO-EM-REACT/avancando_em_react/src/components/ListRender.js
@@ -3,11 +3,20 @@ import { useState } from "react";
 const ListRender = () => {
     const [list] = useState(["Felipe", "Marcos", "Flaubeth"])
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: "Felipe Zeri", age: 31},
-        { id: 203, name: "Felipe Jahnel", age: 20},
-        { id: 302, name: "Felipe", age: 41},
+        { id: 2, name: "Felipe Jahnel", age: 20},
+        { id: 3, name: "Felipe", age: 41},
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4)
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+    }
 
     return (
         <div>
@@ -23,6 +32,7 @@ const ListRender = () => {
                     </li>
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete random user</button>
         </div>
     )
 }
