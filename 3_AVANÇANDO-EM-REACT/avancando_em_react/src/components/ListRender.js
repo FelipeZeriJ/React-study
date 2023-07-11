318794d4c96209c37b1d8b0fc8354d1dcf99c8ce
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-11 13:18:21 -0500

    propriedade key

--- 3_AVANÇANDO-EM-REACT/avancando_em_react/src/components/ListRender.js
+++ 3_AVANÇANDO-EM-REACT/avancando_em_react/src/components/ListRender.js
@@ -3,11 +3,24 @@ import { useState } from "react";
 const ListRender = () => {
     const [list] = useState(["Felipe", "Marcos", "Flaubeth"])
 
+    const [users] = useState([
+        { id: 1, name: "Felipe Zeri", age: 31},
+        { id: 203, name: "Felipe Jahnel", age: 20},
+        { id: 302, name: "Felipe", age: 41},
+    ])
+
     return (
         <div>
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.name} - {user.age}
+                    </li>
                 ))}
             </ul>
         </div>
