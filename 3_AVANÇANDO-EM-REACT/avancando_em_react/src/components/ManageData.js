898a777868dced8e72b0e43e1c7e2ee51d78ae8e
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-11 12:34:40 -0500

    fazendo componente Manage data

--- 3_AVANÇANDO-EM-REACT/avancando_em_react/src/components/ManageData.js
+++ 3_AVANÇANDO-EM-REACT/avancando_em_react/src/components/ManageData.js
@@ -1,6 +1,25 @@
+import {useState} from 'react'
+
 const ManageData = () => {
+    let someData = 10
+
+    console.log(someData)
+
+    // eslint-disable-next-line no-undef
+    const [number, setNumber] = useState(15);
+
+    console.log(number)
     return(
-        <div>ManageData</div>
+        <div>
+            <div>
+                <p>Valor: {someData}</p>
+                <button onClick = {() => (someData = 15)}>Mudar variável</button>
+            </div>
+            <div>
+                <p>Valor: {number}</p>
+                <button onClick={() => setNumber(25)}>Mudar o state</button>
+            </div>
+        </div>
     )
 }
 
