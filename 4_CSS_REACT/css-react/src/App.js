04b2973f40d4d108902f4ee7ff3e9eee434396d4
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-25 13:58:29 -0500

    componentes css

--- 4_CSS_REACT/css-react/src/App.js
+++ 4_CSS_REACT/css-react/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React em CSS</h1>
+
+      {/* CSS de Componente */}
+      <MyComponent />
+      Este parágrafo é do app.js
     </div>
   );
 }
