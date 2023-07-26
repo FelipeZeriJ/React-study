9906a49dc63d2bcbb3641a9f388a7b48937773c7
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-26 08:21:32 -0500

    css inline

--- 4_CSS_REACT/css-react/src/App.js
+++ 4_CSS_REACT/css-react/src/App.js
@@ -10,6 +10,9 @@ function App() {
       {/* CSS de Componente */}
       <MyComponent />
       Este parágrafo é do app.js
+      {/* Inline CSS */}
+      <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+      <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
     </div>
   );
 }
