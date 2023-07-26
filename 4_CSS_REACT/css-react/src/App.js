e93c81a440bb0563df69a1b0fb77b4b6451966f4
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-26 10:17:19 -0500

    modulos do CSS

--- 4_CSS_REACT/css-react/src/App.js
+++ 4_CSS_REACT/css-react/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import { useState } from "react";
+import Title from './components/Title';
 
 function App() {
+  const n = 15
+  const [name] = useState("Felipe")
+
+  const redTitle = true
+  
   return (
     <div className="App">
       {/* CSS Global */}
@@ -10,10 +17,22 @@ function App() {
       {/* CSS de Componente */}
       <MyComponent />
       Este parágrafo é do app.js
-      
+
       {/* Inline CSS */}
       <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
       <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+
+      {/* CSS Inline dinâmico */}
+      <h2 style={ n < 10 ? { color: "purple" } : { color: "pink" }}>CSS dinâmico</h2>
+      <h2 style={ n > 10 ? { color: "purple" } : { color: "pink" }}>CSS dinâmico</h2>
+      <h2 style={ name === "Felipe" ? { color: "green", backgroundColor: "black" } : null}>Teste nome</h2>
+
+      {/* Classe Dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
+
+      {/* CSS Modules */}
+      <Title />
+      <h2 className="my_title">Testando</h2>
     </div>
   );
 }
