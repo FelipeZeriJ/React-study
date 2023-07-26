afab912f00f673e505092d284b71c6935b4b1f05
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-26 10:53:15 -0500

    add novos components

--- 4_CSS_REACT/challengecss/src/App.js
+++ 4_CSS_REACT/challengecss/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CarDetails from './components/CarDetails';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello World!</h2>
+      <CarDetails />
     </div>
   );
 }
