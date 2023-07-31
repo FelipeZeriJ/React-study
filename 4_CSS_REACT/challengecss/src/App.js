14e4455c259f46abc5949604afe31f76d47bbc51
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-31 10:37:20 -0500

    fazendo Tarefa 5 sessão 4 curso react

--- 4_CSS_REACT/challengecss/src/App.js
+++ 4_CSS_REACT/challengecss/src/App.js
@@ -1,11 +1,26 @@
+// App.js
 import './App.css';
 import CarDetails from './components/CarDetails';
 
 function App() {
+  const cars = [
+    { id: 1, modelo: "Ford", km: 0, cor: "Vermelho"},
+    { id: 2, modelo: "Ferrari", km: 0, cor: "Verde"},
+    { id: 3, modelo: "Lamborghini", km: 1000, cor: "Azul"},
+  ]
+
   return (
     <div className="App">
-      <h2>Hello World!</h2>
-      <CarDetails />
+      <h1>Loja de Carros Zerimotivos</h1>
+      <h2>Automotivos Novos e Semi-Novos</h2>
+      {cars.map((car) => (
+        <CarDetails 
+        key={car.id}
+        modelo={car.modelo}
+        km={car.km}
+        cor={car.cor}
+        />
+      ))}
     </div>
   );
 }
--- 4_CSS_REACT/challengecss/src/components/CarDetails.js
+++ 4_CSS_REACT/challengecss/src/components/CarDetails.js
@@ -1,7 +1,15 @@
-const CarDetails = () => {
+// CarDetails.js
+import styles from './CarDetails.module.css';
+
+const CarDetails = ({ modelo, km, cor }) => {
     return (
         <div>
-            <h1>Car Details</h1>
+            <h1 className={styles.my_car_details}>Car Details</h1>
+            <div>
+                <div className={styles.my_car_modelo}>Modelo: {modelo}</div>
+                <div className={styles.my_car_km}>KM: {km}</div>
+                <div className={styles.my_car_cor}>Cor: {cor}</div>
+            </div>
         </div>
     )
 }
