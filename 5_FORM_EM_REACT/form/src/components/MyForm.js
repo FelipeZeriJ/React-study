423fbfcee492be7cf113e744a9cbce0f621b820e
Felipe Zeri <felipezerijahnel@gmail.com>
2023-07-31 13:38:25 -0500

    feat: alteration state inline/Alteração de state inline

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -12,7 +12,8 @@ const MyForm = () => {
     }
 
     console.log(name)
-    
+    console.log(email)
+
     return (
         <div>
             {/* 1 - Criação de Forms */}
@@ -29,7 +30,9 @@ const MyForm = () => {
                 {/* 2 - Label envolvendo input */}
                 <label>
                     <span>E-mail:</span>
-                    <input type="email" name="email" placeholder="Digite o seu e-mail" />
+                    <input type="email" name="email" placeholder="Digite o seu e-mail" 
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
