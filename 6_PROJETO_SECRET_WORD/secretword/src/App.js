baea6abc6e8e75e0de325aa9dbc47de656d6fb53
Felipe Zeri <felipezerijahnel@gmail.com>
2023-08-03 10:03:01 -0500

    Feat: Creation and Settings for game state, valid and invalid letters insertion event and processing

--- 6_PROJETO_SECRET_WORD/secretword/src/App.js
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.js
@@ -26,6 +26,13 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState("");
 
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, SetWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
+
+  console.log(words)
+
   const pickWordAndCategory = () => {
     // Pick a random category
     const categories = Object.keys(words);
@@ -57,14 +64,14 @@ function App() {
     // fill states
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   }
 
   // Process the lette input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   }
 
   // Restarts the Game
@@ -75,7 +82,18 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && (
+        <Game 
+          verifyLetter={verifyLetter} 
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score} 
+        />
+      )}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
